846a3d9b47fd6ebd688a3e3adaeff0e923c10b12
pengyidong <1124666946@qq.com>
2022-04-20 00:04:42 +0800

    feat

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,11 +1,46 @@
 Page({
   data: {
     active: 0,
+    radio: '1',
+    columns: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+    show: false,
   },
   onStudentId(event) {
     console.log(event.detail)
   },
   onPassword(event) {
     console.log(event.detail)
+  },
+  onChange(event) {
+    this.setData({
+      radio: event.detail,
+    });
+  },
+  showPopup() {
+    this.setData({
+      show: true
+    });
+  },
+
+  onClose() {
+    this.setData({
+      show: false
+    });
+  },
+  pickerOnChange(event) {
+    const {
+      picker,
+      value,
+      index
+    } = event.detail;
+  },
+
+  onClick(event) {
+    const {
+      name
+    } = event.currentTarget.dataset;
+    this.setData({
+      radio: name,
+    });
   },
 });
