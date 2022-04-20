e60d86750c28f336405c11ed371fe948e680e1e9
pengyidong <1124666946@qq.com>
2022-04-20 17:36:47 +0800

    feat

---
+++ pages/editStudent/editStudent.js
@@ -0,0 +1,22 @@
+// pages/userDetail/userDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info:[
+      {title:'姓名',value:"邱宇"},
+      {title:'指导老师',value:"高金丽"},
+      {title:'学号',value:"2019666"},
+      {title:'手机号',value:"13257079395"},
+      {title:'班级',value:"软件一班"},
+      {title:'年龄',value:"25"},
+      {title:'身份证号',value:"360724199901120012"},
+      {title:'是否就业',value:"已就业"},
+      {title:'企业名称',value:"特种兵四人小组"},
+      {title:'社会信用代码',value:"9144030071526726XG"},
+      {title:'工作地址',value:"深圳企鹅大厦409"},
+    ]
+  },
+})
