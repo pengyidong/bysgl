42ead7d3d78577e2ce3c13a98c6b918c81459850
pengyidong <1124666946@qq.com>
2022-04-20 18:28:54 +0800

    feat

---
+++ pages/recruitmentDetail/recruitmentDetail.js
@@ -0,0 +1,19 @@
+// pages/recruitmentDetail/recruitmentDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info:{
+      name:"腾讯",
+      info:'腾讯企业信息介绍',
+      requirements:'要求',
+      salary: '10-15k',
+      base: '广州白云区',
+      position: '前端开发',
+      contact:'彭奕东',
+      mobile:'13257079395'
+    }
+  },
+})
